a92568e555a4dfe8db1dfb478dd489e3c4a31ca6
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-09-20 16:36:25 -0500

    Create mainbeta.js

---
+++ mainbeta.js
@@ -0,0 +1,63 @@
+const env = Object.assign(process.env, require("./env"), require("./config"));
+const { app, BrowserWindow, Menu } = require("electron");
+const fs = require("fs");
+const path = require("path");
+
+/**
+ * load flash player
+ */
+let pluginName;
+switch (process.platform) {
+	case "win32": {
+		pluginName = "./extensions/pepflashplayer.dll";
+		break;
+	} case "darwin": {
+		pluginName = "./extensions/PepperFlashPlayer.plugin";
+		break;
+	} case "linux": {
+		pluginName = "./extensions/libpepflashplayer.so";
+		app.commandLine.appendSwitch("no-sandbox");
+		break;
+	}
+}
+app.commandLine.appendSwitch("ppapi-flash-path", path.join(__dirname, pluginName));
+app.commandLine.appendSwitch("ppapi-flash-version", "32.0.0.371");
+
+let mainWindow;
+const createWindow = () => {
+	mainWindow = new BrowserWindow({
+		width: 1200,
+		height: 700,
+		title: "Anistick",
+		icon: path.join(__dirname, "./favicon.ico"),
+		webPreferences: {
+			plugins: true,
+			contextIsolation: true
+		}
+	});
+	// use it in external scripts
+	process.env.MAIN_WINDOW_ID = mainWindow.id;
+
+	// initialize stuff
+	// clear the menu bar
+	Menu.setApplicationMenu(Menu.buildFromTemplate([]));
+	// load the video list
+	mainWindow.loadURL("https://localhost:8140");
+	mainWindow.on("closed", () => mainWindow = null);
+
+	// debug stuff
+	if (env.NODE_ENV == "development") {
+		mainWindow.webContents.openDevTools();
+	}
+};
+
+app.whenReady().then(() => {
+	// wait for the server
+	setTimeout(createWindow, 2000);
+});
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") app.quit();
+});
+app.on("activate", () => {
+	if (mainWindow === null) createWindow();
+});
