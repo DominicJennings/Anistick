135a88f15e059279b1e48dc85102037e23efd954
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-20 21:28:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/* Before you ask me kia, Why did i remove the app window function from this file and replaced it as this was a web app?
+Well, lets just say that things wern't working quite well with the app window as it opens. 
+it came up with errors saying that the url failed to load on an https interface.
+As a bug fix, i added a browser launcher as in you have to open up localhost as chromium opens.
+Pretty cool system, huh? Well, see you next time on dms.
+*/
 const server = () => require("./server");
 const path = require('path');
 const env = Object.assign(process.env, require("./env"), require("./config"));
