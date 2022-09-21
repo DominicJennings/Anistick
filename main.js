b533c9ad56641949d74d3e4888e9ea42f7723b81
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-20 21:31:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ Pretty cool system, huh? Well, see you next time on dms.
 const server = () => require("./server");
 const path = require('path');
 const env = Object.assign(process.env, require("./env"), require("./config"));
-Object.assign(process.body, require("./misc/post_body"));
 const fs = require('fs');
 var chrome;
 // most devs have lvm clones stored in github desktop. so this will change for devs and users.
