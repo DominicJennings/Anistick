9807b947761b4668c65389d573d1352e5c722e13
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-20 20:55:07 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const server = () => require("./server");
 const path = require('path');
-const chrome = path.join(__dirname, "../../../../AppData/Local/Chromium/Application");
+const chrome = path.join(__dirname, "../../../AppData/Local/Chromium/Application");
 var exec = require('child_process').execFile;
 var browser = () => {
 	exec(`${chrome}/chrome.exe`, function(err, data) {  
