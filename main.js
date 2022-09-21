852541010d86f6e0dd67081d72d6e1c0655d4390
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-20 20:54:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,12 @@
-const env = Object.assign(process.env, require("./env"), require("./config"));
-const { app, BrowserWindow, Menu } = require("electron");
-const fs = require("fs");
-const path = require("path");
-require("./server");
-let pluginName;
-switch (process.platform) {
-	case "win32": {
-		pluginName = "./extensions/pepflashplayer.dll";
-		break;
-	} case "darwin": {
-		pluginName = "./extensions/PepperFlashPlayer.plugin";
-		break;
-	} case "linux": {
-		pluginName = "./extensions/libpepflashplayer.so";
-		app.commandLine.appendSwitch("no-sandbox");
-		break;
-	}
+const server = () => require("./server");
+const path = require('path');
+const chrome = path.join(__dirname, "../../../../AppData/Local/Chromium/Application");
+var exec = require('child_process').execFile;
+var browser = () => {
+	exec(`${chrome}/chrome.exe`, function(err, data) {  
+		if (err) console.error(err);
+		else console.log("Launching Chromium...", data);
+	});  
 }
-app.commandLine.appendSwitch("ppapi-flash-path", path.join(__dirname, pluginName));
-app.commandLine.appendSwitch("ppapi-flash-version", "32.0.0.371");
-
-let mainWindow;
-const createWindow = () => {
-	mainWindow = new BrowserWindow({
-		width: 1200,
-		height: 700,
-		title: "Anistick",
-		icon: path.join(__dirname, "./favicon.ico"),
-		webPreferences: {
-			plugins: true,
-			contextIsolation: true
-		}
-	});
-	// use it in external scripts
-	process.env.MAIN_WINDOW_ID = mainWindow.id;
-
-	// initialize stuff
-	// clear the menu bar
-	Menu.setApplicationMenu(Menu.buildFromTemplate([]));
-	// load the video list
-	mainWindow.loadURL("https://localhost:8140");
-	mainWindow.on("closed", () => mainWindow = null);
-
-	// debug stuff
-	if (env.NODE_ENV == "development") {
-		mainWindow.webContents.openDevTools();
-	}
-};
-
-app.whenReady().then(() => {
-	// wait for the server
-	setTimeout(createWindow, 2000);
-});
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") app.quit();
-});
-app.on("activate", () => {
-	if (mainWindow === null) createWindow();
-});
+browser();
+server();
